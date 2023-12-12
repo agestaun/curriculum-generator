2b60c15b6acd98bea57123cad4a054d95cafe9b8
Adrián García <adrian.garcia@entrust.com>
2023-12-12 10:05:52 +0100

    Creating method to return img in base64

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -4,3 +4,9 @@ export const base64Encode = (filePath: string): string => {
 	const bitmap = fs.readFileSync(filePath);
 	return new Buffer(bitmap).toString('base64');
 };
+
+export const getImage = (imgWithExtension: string): string => {
+	const img = new Image();
+	img.src = './assets/' + imgWithExtension;
+	return new Buffer(img.src).toString('base64');
+};
