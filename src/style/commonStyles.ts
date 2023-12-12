b453bc2cd64f063632342e0541137ecda027c9d3
Adrián García <adrian.garcia@entrust.com>
2023-12-12 10:04:52 +0100

    Improving common styles.

--- src/style/commonStyles.ts
+++ src/style/commonStyles.ts
@@ -6,8 +6,23 @@ export const Color = {
 	accent: '#FFDE02',
 };
 
-export const FontSize = {
-	title: 30,
-	subtitle: 14,
-	boy: 16,
+export const font = {
+	size: {
+		title: 30,
+		subtitle: 14,
+		headline: 12,
+		bodyBold: 11,
+		body: 11,
+		bodySmall: 10,
+	}
+};
+
+export const spacing = {
+	xxs: 2,
+	xs: 4,
+	sm: 8,
+	md: 16,
+	lg: 24,
+	xl: 32,
+	xxl: 40,
 };
